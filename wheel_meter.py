bb379eb3b1b8ac96e0ac5c99d4f4084b78cd977a
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2025-02-24 01:58:49 +0100

    Update wheel_meter.py
    Prepared the file as a module for legible.py

--- wheel_meter.py
+++ wheel_meter.py
@@ -4,35 +4,29 @@ from signal import pause
 from time import time, sleep
 from threading import Thread
 from math import pi
+from colorama import Fore, Back, Style
 import numpy as np
 
 # Constants
-PIN = 17 # The GPIO pin used for the sensor's digital output
-BOUNCE_TIME = 0.01 # The time span during which the sensor ingores inputs after a trigger (necessary)
-DEFAULT_DIAMETER = 622 # The default diameter of the wheel (in millimeters)
-AVG_SMOOTHNESS = 5 # The amount of stored previous speed (used to compute a rolling average)
-PERIOD = 2 # The duration between each measuring (in seconds)
+from config import *
 
-print(f"Using pin {PIN}.") 
-wheel_diameter_mm = input("Wheel diameter (millimeters): ")
+# Variables
+wheel_diameter_mm = 0 # The wheel diameter (millimeters)
+circum_m = 0 # The wheel circumference (meters)
+previous_values = np.zeros(AVG_SMOOTHNESS) # The previous values measured
+count = 0 # The number of sensor triggers (resets after each period)
+previous_time = 0 # The previous time (obviously)
 
-try:
-    wheel_diameter_mm = int(wheel_diameter_mm)
-except ValueError:
-    print("Setting diameter to default value (622 mm).")
-    wheel_diameter_mm = DEFAULT_DIAMETER
-
-circum_m = wheel_diameter_mm * pi / 1000
-previous_values = np.zeros(AVG_SMOOTHNESS)
-count = 0
-previous_time = time()
+# Useful values
+speed = 0
+avg_speed = 0
 
 def detected():
     global count
     count += 1
 
 def round_meter():
-    global count, previous_time, previous_values
+    global count, previous_time, previous_values, speed, avg_speed
 
     while True:
         sleep(PERIOD)
@@ -42,14 +36,34 @@ def round_meter():
         speed = circum_m * rounds * 3.6
         
         # Rolling average
-        previous_values = np.roll(previous_values, -1)
-        previous_values[-1] = speed
-        avg = np.average(previous_values)
+        if (USE_AVG_SPEED):
+            previous_values = np.roll(previous_values, -1)
+            previous_values[-1] = speed
+            avg_speed = np.average(previous_values)
 
-        print(f"Rounds (/s): {rounds:.2f} | Speed (km/h): {speed:.2f} | Smoothened speed (km/h): {avg:.2f}")
+        if (__name__) == "__main__":
+            print(f"Rounds (/s): {rounds:.2f} | Speed (km/h): {speed:.2f} | Smoothened speed (km/h): {avg_speed:.2f}")
         
         count = 0
         previous_time = time()
+
+print(f"Using pin {PIN}", end="")
+
+if __name__ == "__main__":
+    wheel_diameter_mm = input("\nWheel diameter (millimeters): ")
+    try:
+        wheel_diameter_mm = int(wheel_diameter_mm)
+    except ValueError:
+        print(f"Setting diameter to default value ({DEFAULT_DIAMETER} mm)")
+        wheel_diameter_mm = DEFAULT_DIAMETER
+else:
+    print(f" | Wheel diameter: {DEFAULT_DIAMETER} mm.")
+    print(Fore.BLUE + "Note: You can change the pin number or the wheel diameter in config.py." + Style.RESET_ALL)
+    wheel_diameter_mm = DEFAULT_DIAMETER
+
+
+circum_m = wheel_diameter_mm * pi / 1000
+previous_time = time()
 
 # Speed measuring thread
 thread = Thread(target=round_meter, daemon=True)
@@ -59,5 +73,6 @@ thread.start()
 sensor = Button(PIN, bounce_time=BOUNCE_TIME)
 sensor.when_pressed = detected
 
-print("Measuring...")
-pause()
+if __name__ == "__main__":
+    print("Measuring...")
+    pause()
