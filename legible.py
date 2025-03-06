d16fb73171246d06b40fc11cd169cccc01fbaf83
Mario Guzman <marioguzzzman@gmail.com>
2025-03-06 16:04:03 +0100

    fade change

--- legible.py
+++ legible.py
@@ -60,7 +60,7 @@ def main():
     }
 
     # Fade rate (volume change per iteration)
-    FADE_RATE = 0.05
+    FADE_RATE = 0.5
 
     # Initialize master volume
     master_volume = DEFAULT_MASTER_VOLUME
